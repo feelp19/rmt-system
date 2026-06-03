f4c880a9088a1531d3c1b79d67dfc6ed81c74f2c
rmt-dev <dev@rmt.local>
2026-06-03 14:16:33 -0300

    feat: authorization gates for horizon, pulse, log-viewer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
+use Opcodes\LogViewer\Facades\LogViewer;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::define('viewPulse', function (?User $user) {
+            return $user?->email === 'flp.pietro19@gmail.com';
+        });
+
+        LogViewer::auth(function ($request) {
+            return $request->user()?->email === 'flp.pietro19@gmail.com';
+        });
     }
 }
